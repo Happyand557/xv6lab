747cb00cafe5c184b20a6b27a850399ecd2bf67d
ryj <972412857@qq.com>
2023-08-26 06:51:09 +0000

    finish  user  function sleep

---
+++ user/sleep.c
@@ -0,0 +1,27 @@
+#include "kernel/types.h"
+#include "user/user.h"
+#include "kernel/types.h"
+
+
+int
+main(int argc, char *argv[]) {
+  char *iter;
+
+  if(argc == 1) {
+    fprintf(2, "sleep argc can not be empty\n");
+    exit(-1);
+  } else if(argc > 2) {
+    fprintf(2, "sleep given too much args\n");
+    exit(-1);
+  }
+  
+  for(iter = argv[1]; *iter != '\0'; iter++) {
+    if(*iter > '9' || *iter < '0') {
+        fprintf(2, "%s is not a valid arg\n", argv[1]);
+        exit(-1);
+    }
+  }
+
+  sleep(atoi(argv[1]));
+  exit(0);
+}
